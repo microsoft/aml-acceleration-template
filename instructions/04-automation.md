914cd9d3d534ca79f359232b6c97c7bdeac6f792
alex zeltov <azeltov@gmail.com>
2020-05-14 11:14:27 -0400

    Azure Devops MLOPS automation (#6)
    * Set up CI with Azure Pipelines
    
    [skip ci]
    
    * start dev
    
    * integtate cicd to python pipeline
    
    * load runconfig from file ci/cd
    
    * disable run pipeline
    
    * cicd work for pipeline
    
    * dev
    
    * fix bugs
    
    * cleanup
    
    * dev
    
    * documentation
    
    * cleanup git
    
    * Clean up ignored files
    
    * cleanup
    
    * cleanup
    
    * sync to master
    
    * Clean up ignored files
    
    * compute create using cli
    
    * cleanup
    
    * refactor based on Clemens feedback
    
    * refactor path to new folder
    
    * fix
    
    * fix cluster name
    
    * rename
    
    Co-authored-by: Alex Zeltov <alzeltov@microsoft.com>

--- instructions/04-automation.md
+++ instructions/04-automation.md
@@ -1,3 +1,73 @@
-# Automation
+### Create New Project
 
-To be written
+1. Sign in to [Azure DevOps](http://dev.azure.com)
+
+2. Select **Create project**
+
+3. Provide Project Name: `aml-acceleration-template` and select **Create**
+
+    
+###  Import Quickstart code from a Github Repo
+
+1. Within the new project:
+
+   a. Select **Repos** from left navigation bar
+   
+   b. Select **Import** from the content section
+   
+   
+2. Provide the Github URL of your cloned aml_accelarator template instance and select **Import**. 
+
+
+### Update the build YAML file
+
+1. Select and open the `azure-pipelines.yml` file
+
+2. Select **Edit** and update the following variables: `resourcegroup`, and `workspace`. 
+
+3. Select **Commit** to save your changes.
+
+  
+### Create new Service Connection
+
+1. From the left navigation select **Project settings** and then select **Service connections**
+
+2. Select **New service connection** and then select **Azure Resource Manager**
+
+3. Provide the following information in the `Add an Azure Resource Manager service connection` dialog box and then select **Ok**:
+ 
+   a. Connection name: `aml_accelarator_svc_conn`
+   
+   b. Subscription: Select the Azure subscription to use
+   
+   c. Resource Group: This value should match the value you provided in the `azure-pipelines.yml` file
+
+### Create/Verify Dataset in AML 
+
+1. Follow instruction on [Create Dataset in AML from Training Page](https://github.com/microsoft/aml-acceleration-template/blob/master/instructions/01-training.md)
+
+### Setup Build Pipeline
+
+1. From left navigation select **Pipelines, Builds** and then select **New pipeline**
+    
+2. Select **Azure Repos Git** as your code repository
+
+3. Select **aml_accelarator** as your repository
+
+4. Review the YAML file
+
+    The build pipeline has four key steps:
+    
+    a. Attach folder to workspace and experiment. 
+    
+    b. Create the AML Compute target to run your master pipeline for model training and model evaluation.
+    
+    c. Run the master pipeline. 
+    
+    d. Publish the build artifacts. 
+
+### Run the Build Pipeline
+
+1. Select **Run** to start running your build pipeline
+
+2. Monitor the build run. 
--- pipelines-python/train/pipeline.py
+++ pipelines-python/train/pipeline.py
@@ -1,27 +1,75 @@
 import os
+import argparse
 import azureml.core
 from azureml.core import Workspace, Experiment, Datastore, Dataset, RunConfiguration
-
-from azureml.pipeline.core import Pipeline
+from azureml.core.compute import AmlCompute
+from azureml.core.compute import ComputeTarget
+from azureml.pipeline.core import Pipeline, PipelineData
 from azureml.pipeline.steps import PythonScriptStep
+from azureml.core.authentication import AzureCliAuthentication
+print("Pipeline SDK-specific imports completed")
+# Check core SDK version number
+print("Azure ML SDK version:", azureml.core.VERSION)
 
-dataset_name =  os.getenv('DATASET', 'german-credit-filedataset')
-dataset_mountpath = os.getenv('DATASET_MOUNTPATH', '/data')
-source_directory = os.getenv('SOURCE_DIRECTORY', '../../src/model1/')
-runconfig = os.getenv('RUNCONFIG', 'pipeline.runconfig')
+parser = argparse.ArgumentParser("pipelines_master")
+parser.add_argument("--model_name", type=str, help="model name", dest="model_name", required=True)
+parser.add_argument("--build_number", type=str, help="build number", dest="build_number", required=False)
+parser.add_argument("--image_name", type=str, help="image name", dest="image_name", required=True)
+parser.add_argument("--path", type=str, help="path", dest="path", required=True)
+parser.add_argument("--dataset", type=str, help="dataset", dest="dataset", required=True)
+parser.add_argument("--dataset_mountpath", type=str, help="dataset_mountpath", dest="dataset_mountpath", required=True)
+parser.add_argument("--runconfig", type=str, help="runconfig", dest="runconfig", required=True)
+parser.add_argument("--source_directory", type=str, help="source_directory", dest="source_directory", required=True)
+args = parser.parse_args()
 
-print("SDK version:", azureml.core.VERSION)
+#dataset_name =  os.getenv('DATASET', 'german-credit-filedataset')
+#dataset_mountpath = os.getenv('DATASET_MOUNTPATH', '/data')
+#source_directory = os.getenv('SOURCE_DIRECTORY', '../../src/model1/')
+#runconfig = os.getenv('RUNCONFIG', 'pipeline.runconfig')
 
-ws = Workspace.from_config()
-print(ws.name, ws.resource_group, ws.location, ws.subscription_id, sep = '\n')
 
-input_ds = Dataset.get_by_name(ws, dataset_name)
-training_data = input_ds.as_named_input('training_dataset').as_mount(path_on_compute=dataset_mountpath)
+print("Argument source_directory %s" % args.source_directory)
+print("Argument model_name: %s" % args.model_name)
+print("Argument build_number: %s" % args.build_number)
+print("Argument image_name: %s" % args.image_name)
+print("Argument path: %s" % args.path)
+print("Argument dataset: %s" % args.dataset)
+print("Argument dataset_mountpath: %s" % args.dataset_mountpath)
+print("Argument runconfig: %s" % args.runconfig)
 
-runconfig = RunConfiguration.load(runconfig)
+print('creating AzureCliAuthentication...')
+cli_auth = AzureCliAuthentication()
+print('done creating AzureCliAuthentication!')
+
+print('get workspace...')
+ws = Workspace.from_config(path=args.path, auth=cli_auth)
+print('Workspace name: ' + ws.name, 
+      'Azure region: ' + ws.location, 
+      'Subscription id: ' + ws.subscription_id, 
+      'Resource group: ' + ws.resource_group, sep = '\n')
+      
+print('done getting workspace!')
+
+
+
+scripts_folder = 'scripts'
+def_blob_store = ws.get_default_datastore()
+
+train_output = PipelineData('train_output', datastore=def_blob_store)
+print("train_output PipelineData object created")
+
+print('--------------------------')
+print(args.runconfig)
+runconfig = RunConfiguration.load(args.runconfig)
+print('--------------------------')
+
+print("setup dataset")    
+input_ds = Dataset.get_by_name(ws, args.dataset)
+training_data = input_ds.as_named_input('training_dataset').as_mount(path_on_compute=args.dataset_mountpath)
+
 
 train_step = PythonScriptStep(name="train-step",
-                        source_directory=source_directory,
+                        source_directory=args.source_directory,
                         runconfig=runconfig,
                         inputs=[training_data],
                         script_name=runconfig.script,
@@ -32,9 +80,9 @@ steps = [train_step]
 
 pipeline = Pipeline(workspace=ws, steps=steps)
 pipeline.validate()
+print("pipeline validation complete")
+#pipeline_run = Experiment(ws, 'train-pipe').submit(pipeline)
+#pipeline_run.wait_for_completion()
 
-pipeline_run = Experiment(ws, 'train-pipe').submit(pipeline)
-pipeline_run.wait_for_completion()
-
-#published_pipeline = pipeline.publish(pipeline_name)
-#print("Published pipeline id: ", published_pipeline.id)
+published_pipeline = pipeline.publish(args.model_name)
+print("Published pipeline id: ", published_pipeline.id)
